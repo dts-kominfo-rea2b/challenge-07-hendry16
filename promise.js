94c641a1a07d98a01c6cfd44796a8cb31138d532
Hendri Sibarani <hendrisibarani15@gmail.com>
2022-06-12 22:31:16 +0700

    done

--- promise.js
+++ promise.js
@@ -1,7 +1,45 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve, reject) => {
+    if (emosi == "") {
+      reject("Invalid Data!");
+    } else if (emosi == "marah") {
+      (async () => {
+        const sumIXX = count(await promiseTheaterIXX(), emosi);
+        const sumVGC = count(await promiseTheaterVGC(), emosi);
+        resolve(sumIXX + sumVGC);
+      })();
+    } else if (emosi == "tidak marah") {
+      (async () => {
+        const sumIXX = count(await promiseTheaterIXX(), emosi);
+        const sumVGC = count(await promiseTheaterVGC(), emosi);
+        resolve(sumIXX + sumVGC);
+      })();
+    }
+  });
+};
+
+const count = (data, feel) => {
+  let counter = 0;
+  if (feel == "marah") {
+    for (let i=0; i<data.length; i++) {
+      if (data[i].hasil == "marah") {
+        counter++;
+      }
+    }
+  } else {
+    for (let i=0; i<data.length; i++) {
+      if (data[i].hasil == "tidak marah") {
+        counter++;
+      }
+    }
+  }
+  return counter;
+};
+
+
 
 module.exports = {
   promiseOutput,
